9d45c29e2367926623db697b415fbf3ae4b8d418
andrejdergavko <andrejdergavko@gmail.com>
2026-04-21 11:00:19 +0300

    Update health check status in AppService to 'ok1' for improved clarity.

--- src/app.service.ts
+++ src/app.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@nestjs/common';
 export class AppService {
   getHealth() {
     return {
-      status: 'ok',
+      status: 'ok1',
       timestamp: new Date().toISOString(),
     };
   }
